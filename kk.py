fe057d6bcd2b2edd407b9c949c1eac8bd0ec530f
phillipyu <phillip.yu.1@gmail.com>
2017-04-20 03:57:41 -0400

    added comments

--- kk.py
+++ kk.py
@@ -7,16 +7,19 @@ import math
 import time
 import sys
 
-MAX_ITER = 25000
-SIZE = 100
-MAX_INT = 10 ** 12
+MAX_ITER = 25000 # number of iterations to run our 3 randomized algorithms
+SIZE = 100 # size of problem
+MAX_INT = 10 ** 12 # maximum integer size in problems
 
+# Common class contains the functions specific to either the pre-partitioning or standard representation 
+# solution form. Common is used to dynamically call these specific functions in our later implementation
+# of the three randomized algorithms.
 class Common(object):
-  # generates random solution of length SIZE for prepartitioning
+  # Returns random solution of length SIZE for prepartitioning.
   def rsol_pp(self):
     return [randint(0, SIZE - 1) for i in xrange(SIZE)]
 
-  # generates random solution for standard representation
+  # Returns random solution of length SIZE for standard representation.
   def rsol_ss(self):
     S = []
     for i in xrange(SIZE):
@@ -26,19 +29,19 @@ class Common(object):
       S.append(rand)
     return S
 
-  # turns pre-partitioned solution into standard form solution
-  # returns residue
+  # Converts pre-partitioned solution into standard form solution.
+  # Returns residue obtained by running KK on this solution.
   def residue_pp(self, P, A):
     A_prime = [0] * len(A)
     for i in range(0,len(A)):
       A_prime[P[i]] += A[i]
     return kk(A_prime)
 
-  # returns residue of A given standard-representation of solution S
+  # Returns residue of list A given standard-representation of solution S.
   def residue_ss(self, S, A):
     return abs(sum([x * y for x, y in zip(S, A)]))
 
-  # generates random neighbor for pre-partitioned representation (list differing in one spot)
+  # Returns random neighbor (defined as a solution differing in one spot) for pre-partitioned solution.
   def rneighbor_pp(self, P):
     P_prime = P[:]
     i = randint(0, len(P_prime) - 1)
@@ -48,7 +51,7 @@ class Common(object):
     P_prime[i] = j
     return P_prime
 
-  # generates random neighbor for standard solution representation (list differing in one or two spots)
+  # Returns random neighbor (defined as a solution differing in one or two spots) for standard rep. solution.
   def rneighbor_ss(self, S):
     S_prime = S[:]
     i = randint(0, len(S) - 1)
@@ -60,14 +63,11 @@ class Common(object):
       S_prime[j] *= -1
     return S_prime
 
-
-# generates random problem i.e. list of length SIZE
+# Returns random problem i.e. list of length SIZE.
 def rprob():
   return [randint(1, MAX_INT) for i in xrange(SIZE)]
 
-# runs Karmankar-Karp algorithm on list A of integers 
-# returns residue
-# note heapq is min heap
+# Runs Karmankar-Karp algorithm on problem A. Returns the residue obtained. 
 def kk(A):
   A_prime = [-elt for elt in A]
   
@@ -79,12 +79,13 @@ def kk(A):
   res = -first
   return res
 
-# cooling function given in problem set
+# Cooling function, defined in specs of the problem set.
 def cooling(iter):
   expo = math.floor(iter / 300)
   return (10 ** 10) * (.8 ** expo)
 
-# repeated random
+# Repeated random: given partitioning problem A, randomly generates solutions to A. 
+# Returns the best solution over all iterations. 
 def rr(A, method):
   common = Common()
   rsol = getattr(common, "rsol_%s" % method)
@@ -97,7 +98,8 @@ def rr(A, method):
       S = S_prime
   return S
 
-# hill climbing
+# Hill climbing: given partitioning problem A, randomly generates solution and steps to find better neighbors.
+# Returns the final solution that is reached.
 def hc(A, method):
   common = Common()
   rsol = getattr(common, "rsol_%s" % method)
@@ -111,7 +113,8 @@ def hc(A, method):
       S = S_prime
   return S
 
-# simulated annealing
+# Simulated annealing: given partitioning problem A, randomly generates solution and steps to find neighbors (not always better).
+# Returns the best neighbor found across all iterations.
 def sa(A, method):
   common = Common()
   rsol = getattr(common, "rsol_%s" % method)
@@ -134,9 +137,9 @@ def sa(A, method):
       prime2_res = prime_res
   return S_2prime
 
-# given an algorithm func, list A, and file output, computes solution to A using func
-# writes time and residue to output file
-# returns time taken and solution residue
+# Given a randomized algorithm func, list A, file output, and method "pp" or "ss".
+# Computes solution to A using func and representation method, writes time and residue to output.
+# Returns time taken and the residue of the solution.
 def time_func(func, A, output, method):
   common = Common()
   residue = getattr(common, "residue_%s" % method)
@@ -147,7 +150,7 @@ def time_func(func, A, output, method):
   output.write(str(t1 - t0) + " " + str(residue(solution, A)) + " // ") 
   return ((t1 - t0), residue(solution, A))
 
-# generates random file of 100 ints, each on a new line
+# TESTING FUNCTION: Generates random file of 100 ints, each on a new line.
 def random_input():
   random = open("random.txt", "w")
   for i in range(SIZE):
@@ -156,57 +159,66 @@ def random_input():
   return random
 
 
-"""----- PART ONE: Given input file of 100 integers, outputs residue -----"""
-# FOR TESTING: writes random file of 100 integers
-inputfile = random_input().name
+"""
+----- PART ONE: Given input file of 100 integers, outputs residue -----
+"""
+# FOR TESTING: writes random file of 100 integers.
+# inputfile = random_input().name
 
-# gets command line argument input file
-# inputfile = sys.argv[1]
+# Gets command line's inputfile.
+try:
+  inputfile = sys.argv[1]
+except:
+  print "Usage: ./kk.py <inputfile>"
+  sys.exit(1)
 
-# stores problem A as problem given by input file
+# Stores problem A as the problem given by the input file.
 with open(inputfile) as f:
   A = f.readlines() 
   A = [int(x.strip()) for x in A] 
 
-# outputs residue obtained by running KK
+# Prints residue obtained by running KK on A.
 print "Residue for given input file:", kk(A) 
 
 
 
-"""----- PART TWO: Generates 100 random instances of the problem, runs three algorithms on each -----"""
-niter = 100
+"""
+----- PART TWO: Generates 100 random instances of the problem, runs three algorithms on each -----
+Uncomment below to run.
+"""
 
-# opens file to write data to
-output = open("output.txt", "w")
-output.write("FORMAT: Iteration: KK residue // RR SS time, RR SS residue // HC SS time, HC SS residue // SA SS time, SA SS residue // \
-  RR PP time, RR PP residue // HC PP time, HC PP residue // SA PP time, SA PP residue \n") 
+# # Number of problems to generate.
+# niter = 100
 
-# runs code for niter iterations
-t = time.time()
+# # Output file to write data to.
+# output = open("output.txt", "w")
+# output.write("FORMAT: Iteration: KK residue // RR SS time, RR SS residue // HC SS time, HC SS residue // SA SS time, SA SS residue // \
+#   RR PP time, RR PP residue // HC PP time, HC PP residue // SA PP time, SA PP residue \n") 
 
-for i in range(0, niter):
-  # generates random problem A
-  A = rprob()
+# t = time.time()
 
-  print "This is iteration", i  
+# for i in range(0, niter):
+#   A = rprob()
 
-  output.write(str(i) + ": ") 
-  output.write(str(kk(A)) + " // ")
-  time_func(rr, A, output, "ss")
-  time_func(hc, A, output, "ss")
-  time_func(sa, A, output, "ss")
+#   print "This is iteration", i  
 
-  time_func(rr, A, output, "pp")
-  time_func(hc, A, output, "pp")
-  time_func(sa, A, output, "pp")
-  output.write("\n")
+#   output.write(str(i) + ": ") 
+#   output.write(str(kk(A)) + " // ")
+#   time_func(rr, A, output, "ss")
+#   time_func(hc, A, output, "ss")
+#   time_func(sa, A, output, "ss")
 
-tz = time.time()
+#   time_func(rr, A, output, "pp")
+#   time_func(hc, A, output, "pp")
+#   time_func(sa, A, output, "pp")
+#   output.write("\n")
 
-print "Total time taken: ", (tz - t)
-output.write("Total time taken: " + str(tz - t) + "\n")
+# tz = time.time()
 
-output.close() 
+# print "Total time taken: ", (tz - t)
+# output.write("Total time taken: " + str(tz - t) + "\n")
+
+# output.close() 
 
 
 
